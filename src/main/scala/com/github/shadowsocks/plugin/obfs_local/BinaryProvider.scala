643e2d31c52f522f87c3d0ea420a9bad3b081f4a
Max Lv <max.c.lv@gmail.com>
2017-01-19 17:01:40 +0800

    Rename obfs-local to obs_local to avoid naming issues

--- src/main/scala/com/github/shadowsocks/plugin/obfs_local/BinaryProvider.scala
+++ src/main/scala/com/github/shadowsocks/plugin/obfs_local/BinaryProvider.scala
@@ -10,10 +10,10 @@ import com.github.shadowsocks.plugin.{NativePluginProvider, PathProvider}
   * @author Mygod
   */
 final class BinaryProvider extends NativePluginProvider {
-  override protected def populateFiles(provider: PathProvider): Unit = provider.addPath("obfs-local")
+  override protected def populateFiles(provider: PathProvider): Unit = provider.addPath("obfs_local")
 
   override def openFile(uri: Uri): ParcelFileDescriptor = uri.getPath match {
-    case "/obfs-local" => ParcelFileDescriptor.open(
+    case "/obfs_local" => ParcelFileDescriptor.open(
       new File(getContext.getApplicationInfo.nativeLibraryDir + "/libobfs-local.so"),
       ParcelFileDescriptor.MODE_READ_ONLY)
     case _ => throw new FileNotFoundException()
